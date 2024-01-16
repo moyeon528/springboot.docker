14511bd2f0f5def502fb9843928d224b87969378
moyeon <vryehdus0528@gmail.com>
2024-01-16 20:47:15 +0900

    feat: project build

---
+++ src/main/java/com/example/demo/AwsTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsTestApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AwsTestApplication.class, args);
+	}
+
+}
