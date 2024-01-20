c59caa096282d2a7b5d5cd54d1b3dff73e5036ec
moyeon <vryehdus0528@gmail.com>
2024-01-20 18:53:58 +0900

    feat: project build / sqs 디펜던시 삭제 class 주석

--- src/main/java/com/example/demo/AmazonSQSConfig.java
+++ src/main/java/com/example/demo/AmazonSQSConfig.java
@@ -1,50 +1,50 @@
-package com.example.demo;
-
-import com.fasterxml.jackson.databind.ext.SqlBlobSerializer;
-import io.awspring.cloud.sqs.config.SqsMessageListenerContainerFactory;
-import io.awspring.cloud.sqs.operations.SqsTemplate;
-import io.awspring.cloud.sqs.support.converter.SqsMessageConversionContext;
-import lombok.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.sqs.SqsAsyncClient;
-
-@Configuration
-public class AmazonSQSConfig {
-//    @Value("${cloud.aws.region.static}")
-    private String region;
-
-//    @Value("${cloud.aws.credentials.access-key}")
-    private String awsAccessKey;
-
-//    @Value("${cloud.aws.credentials.secret-key}")
-    private String awsSecretKey;
-
-
-    @Bean
-    public SqsAsyncClient sqsAsyncClient(){
-        return  SqsAsyncClient.builder()
-                .credentialsProvider
-                        (StaticCredentialsProvider.create
-                                (AwsBasicCredentials.create
-                                        (awsAccessKey, awsSecretKey)))
-                .region(Region.of(region)).build();
-    }
-
-    @Bean
-    public SqsMessageListenerContainerFactory<Object> defaultSqsListnerContainerFactory(){
-        return SqsMessageListenerContainerFactory
-                .builder()
-                .sqsAsyncClient(sqsAsyncClient())
-                .build();
-    }
-
-    @Bean
-    public SqsTemplate sqsTemplate(){
-        return SqsTemplate.newTemplate(sqsAsyncClient());
-    }
-}
+//package com.example.demo;
+//
+//import com.fasterxml.jackson.databind.ext.SqlBlobSerializer;
+//import io.awspring.cloud.sqs.config.SqsMessageListenerContainerFactory;
+//import io.awspring.cloud.sqs.operations.SqsTemplate;
+//import io.awspring.cloud.sqs.support.converter.SqsMessageConversionContext;
+//import lombok.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+//import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+//import software.amazon.awssdk.regions.Region;
+//import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+//
+//@Configuration
+//public class AmazonSQSConfig {
+////    @Value("${cloud.aws.region.static}")
+//    private String region;
+//
+////    @Value("${cloud.aws.credentials.access-key}")
+//    private String awsAccessKey;
+//
+////    @Value("${cloud.aws.credentials.secret-key}")
+//    private String awsSecretKey;
+//
+//
+//    @Bean
+//    public SqsAsyncClient sqsAsyncClient(){
+//        return  SqsAsyncClient.builder()
+//                .credentialsProvider
+//                        (StaticCredentialsProvider.create
+//                                (AwsBasicCredentials.create
+//                                        (awsAccessKey, awsSecretKey)))
+//                .region(Region.of(region)).build();
+//    }
+//
+//    @Bean
+//    public SqsMessageListenerContainerFactory<Object> defaultSqsListnerContainerFactory(){
+//        return SqsMessageListenerContainerFactory
+//                .builder()
+//                .sqsAsyncClient(sqsAsyncClient())
+//                .build();
+//    }
+//
+//    @Bean
+//    public SqsTemplate sqsTemplate(){
+//        return SqsTemplate.newTemplate(sqsAsyncClient());
+//    }
+//}
